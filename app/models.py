d1bf5a563a4e2c51dfc1e2c35ff6f1b9df2df390
Henning Kutz <henning.kutz@campus.tu-berlin.de>
2016-05-18 14:36:41 +0200

    Added rudimentary model for category in models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(verbose_name="preview image for category", )
+    description = models.TextField(max_length=280)
