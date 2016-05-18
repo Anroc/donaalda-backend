3c4545a7dbc980107f95815d30da26c22c07e59e
Henning Kutz <henning.kutz@campus.tu-berlin.de>
2016-05-18 15:18:21 +0200

    Added scenario, productSet and product to models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,30 @@ from django.db import models
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=50)
-    picture = models.ImageField(verbose_name="preview image for category", )
-    description = models.TextField(max_length=280)
+    name = models.CharField(max_length=100)
+    picture = models.ImageField(verbose_name="image for category", )
+    description = models.TextField()
+    categories = models.ManyToManyField("Scenario")
+
+
+class Scenario(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    picture = models.ImageField(verbose_name="image for scenario", )
+    #provider = models.ForeignKey("Provider")
+
+class ProductSet(models.Model):
+    name = models.CharField(blank=True, max_length=100)
+    description = models.TextField(blank=True, )
+    products = models.ManyToManyField("Product")
+    #provider = models.ForeignKey("Provider")
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image1 = models.ImageField()
+    image2 = models.ImageField(null=True)
+    image3 = models.ImageField(null=True)
+    thumbnail = models.ImageField()
+    end_of_Life = models.BooleanField(default=False)
