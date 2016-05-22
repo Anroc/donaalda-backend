b72a132c9a25d8b6a3e94d2cac2f1bf1ea99d73b
michaelch <michael.chlepakov@gmail.com>
2016-05-22 10:34:27 +0200

    -Copied frontend html into \'backend/advisor/app/templates/app\' and css/js into \'/backend/advisor/app/static/app\'. \n-Modified index_frontend.html to include django template language and added dynamic generation of category fields from corresponding database entries. \nCreated static and template directories. \n-Changed django views to generic format. \-Changed django url redirection to correspond with new views.

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from . import views
 
+app_name = 'app'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index_frontend'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Category, Scenario
 
 
-def index(request):
-    return HttpResponse("This is the startpage of our advisor")
+# def index(request):
+#    return HttpResponse("This is the startpage of our advisor")
+
+
+class IndexView(generic.ListView):
+    template_name = 'app/index_frontend.html'
+    context_object_name = 'latest_category_list'
+
+    def get_queryset(self):
+        return Category.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Scenario
+    template_name = 'app/detail.html'
+
+    def get_queryset(self):
+        return Scenario.objects.all()
