b896b42fe62d9aaceea341236d9df5e4353b1860
Henning Kutz <henning.kutz@campus.tu-berlin.de>
2016-05-28 18:48:42 +0200

    addtional work on model reducing possible inconsistencies

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,66 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Category)
-admin.site.register(Scenario)
-admin.site.register(ProductSet)
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductSetAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        user = request.user
+        qs = super(ProductSetAdmin, self).get_queryset(request)
+
+        if user.is_superuser:
+            return qs
+        return qs.filter(creator=user.employee.employer_id)
+
+
+
+class ScenarioAdmin(admin.ModelAdmin):
+
+    exclude = ["provider"]
+
+    def get_queryset(self, request):
+        user = request.user
+        qs = super(ScenarioAdmin, self).get_queryset(request)
+
+        if user.is_superuser:
+            print("is superuser")
+            return qs
+        print("%s" % user.username)
+        print("%s" % user.pk)
+        print("%s" % user.employee.pk)
+        print("%s" % user.employee.employer_id)
+        return qs.filter(pk=user.employee.employer_id)
+
+'''
+class ProductAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return get_query_set(ProductAdmin, self, request)
+
+'''
+
+
+class ProviderProfileManager(admin.ModelAdmin):
+
+    # exclude = ["owner"]
+    readonly_fields = ["owner"]
+
+    def get_queryset(self, request):
+        user = request.user
+        qs = super(ProviderProfileManager, self).get_queryset(request)
+
+        if user.is_superuser:
+            return qs
+        return qs.filter(owner=user.employee.employer_id)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Scenario, ScenarioAdmin)
+admin.site.register(ProductSet, ProductSetAdmin)
 admin.site.register(Product)
+admin.site.register(Employee)
+admin.site.register(Provider)
+admin.site.register(ProviderProfile, ProviderProfileManager)
+admin.site.register(ProductType)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 # TODO: create own model for user login, short google search: best practice to not let users access admin panel
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -26,8 +30,8 @@ class Scenario(models.Model):
     short_description = models.TextField(verbose_name="Kurzbeschreibung", max_length="255", null=True, blank=True)
     description = models.TextField(verbose_name="Beschreibung")
     picture = models.ImageField(verbose_name="Bild", null=True, blank=True)
-
-    # provider = models.ForeignKey("Provider")
+    provider = models.ForeignKey("Provider", default="1", )
+    scenario_product_set = models.ForeignKey("ProductSet", null=True)
 
     def __str__(self):
         return '%s' % self.name
@@ -41,11 +45,10 @@ class ProductSet(models.Model):
     name = models.CharField(blank=True, max_length=100)
     description = models.TextField(blank=True, verbose_name="Beschreibung")
     products = models.ManyToManyField("Product", verbose_name="Dazugehörige Produkte")
-
-    # provider = models.ForeignKey("Provider")
+    creator = models.ForeignKey("Provider", default="1")
 
     def __str__(self):
-        return ' %s: %s' % (self.id, self.name)
+        return '%s' % (self.name)
 
     class Meta:
         verbose_name = "Produktsammlung"
@@ -54,16 +57,74 @@ class ProductSet(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
+    provider = models.ForeignKey("Provider", default="1")
+    product_type = models.ForeignKey("ProductType", default="0")
+    serial_number = models.CharField(max_length=255, default="------")
     description = models.TextField()
     image1 = models.ImageField()
-    image2 = models.ImageField(null=True)
-    image3 = models.ImageField(null=True)
-    thumbnail = models.ImageField()
-    end_of_Life = models.BooleanField(default=False)
+    image2 = models.ImageField(null=True, blank=True)
+    image3 = models.ImageField(null=True, blank=True)
+    thumbnail = ImageSpecField(source='image1',
+                               processors=[ResizeToFill(200, 100)],
+                               format='JPEG',)
+    end_of_life = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s' % self.name
+
+    def delete(self, using=None, keep_parents=True):
+        self.end_of_life = True
+        self.save()
+
+    class Meta:
+        verbose_name = "Produkt"
+        verbose_name_plural = "Produkte"
+        # unique_together = (("provider", "serial_number"),) # It seems like unique_together does not work with ForeignKey
+
+
+class ProductType(models.Model):
+    type_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s' % self.type_name
+
+    class Meta:
+        verbose_name = "Produktart"
+        verbose_name_plural = "Produktarten"
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=200, unique=True, )
 
     def __str__(self):
         return '%s' % self.name
 
     class Meta:
-        verbose_name = "Produkt"
-        verbose_name_plural = "Produkte"
+        verbose_name = "Hersteller"
+        verbose_name_plural = "Hersteller"
+
+
+class ProviderProfile(models.Model):
+    public_name = models.CharField(max_length=200, unique=True,)
+    logo_image = models.ImageField()
+    banner_image = models.ImageField()
+    introduction = models.TextField()
+    contact_email = models.EmailField()
+    website = models.URLField()
+    owner = models.OneToOneField("Provider", default="1")
+
+    def __str__(self):
+        return '%s' % self.public_name
+
+
+    class Meta:
+        verbose_name = "Herstellerprofil"
+        verbose_name_plural = "Herstellerprofile"
+
+
+class Employee(User):
+    employer = models.ForeignKey("Provider")
+
+    class Meta:
+        verbose_name = "Angestellter"
+        verbose_name_plural = "Angestellte"
