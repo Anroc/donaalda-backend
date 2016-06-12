37878f4023071a36bc8538258e22d275862989df
Tim <maleska@campus.tu-berlin.de>
2016-06-12 19:26:54 +0200

    changed position for google map and removed s

--- app/static/app/assets/js/google_map.js
+++ app/static/app/assets/js/google_map.js
@@ -1,17 +1,17 @@
-
 var google;
 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(52.512991, 13.320043);
     // 39.399872
     // -8.224454
+    // Berlin: 52.512991, 13.320043
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 13,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -30,7 +30,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Brooklyn'];
+    var addresses = ['Ernst-Reuter-Platz 7, 10587 Berlin'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
@@ -39,7 +39,8 @@ function init() {
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/loc.png'
+                //icon: 'images/loc.png'
+                icon: 'static/app/assets/images/loc.png'
             });
 
         });
