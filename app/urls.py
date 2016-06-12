5624dd29900dd646dc5d3c1ea8cbaebe160b20ff
Henning Kutz <henning.kutz@campus.tu-berlin.de>
2016-06-12 19:39:23 +0200

    redirect /app/scenarios/ to /scenarios/

--- app/urls.py
+++ app/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^login$', views.login_view, name='login'),
     url(r'^register$', views.register_user, name='register_user'),
     url(r'^logout$', views.log_out, name='logout'),
-    url(r'^app/scenarios/$', views.ScenariosView.as_view(), name='scenarios'),
+    url(r'^scenarios/$', views.ScenariosView.as_view(), name='scenarios'),
     url(r'^app/(?P<category_name>[\w]+)/$', views.CategoryView.as_view(), name='category'),
     url(r'^app/scenarios/(?P<current_scenario>[\w]+)/$',
         views.ScenarioView.as_view(), name='scenario'),
