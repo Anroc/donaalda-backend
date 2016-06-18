807c5d2379c222af3540c8f297d12ceffe2e9f92
michaelch <michael.chlepakov@gmail.com>
2016-06-19 00:49:30 +0200

    -Removed django_bootstrap_carousel from requirements.txt

--- advisor/settings.py
+++ advisor/settings.py
@@ -85,11 +85,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'DBNAME',
-        # 'USER': 'USERNAME',
-        # 'PASSWORD': 'PASSWORD',
-        # 'HOST': 'LOCALHOST',
-        # 'PORT': '',
+        # 'NAME': 'advisor',
+        # 'USER': 'django',
+        # 'PASSWORD': 'djangoDBpw',
+        # 'HOST': 'dailab-advisor.cgxenmplsqo5.eu-central-1.rds.amazonaws.com',
+        # 'PORT': '5432',
     }
 }
 
