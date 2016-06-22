de1fc4c6dc05d0c640e746548ba6ff6c12f520e1
Michael Chlepakov <michael.chlepakov@gmail.com>
2016-06-22 14:10:27 +0200

    -DATABASE NOW IN AWS

--- advisor/settings.py
+++ advisor/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'advisor.wsgi.application'
 # TODO: change to postgre once we have our server
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'advisor',
-        # 'USER': 'django',
-        # 'PASSWORD': 'djangoDBpw',
-        # 'HOST': 'dailab-advisor.cgxenmplsqo5.eu-central-1.rds.amazonaws.com',
-        # 'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'advisor',
+        'USER': 'django',
+        'PASSWORD': 'djangoDBpw',
+        'HOST': 'dailab-advisor.cgxenmplsqo5.eu-central-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
