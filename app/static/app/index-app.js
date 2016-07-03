3b571393becf57706b1498b485cf88de24bf05d8
michaelch <michael.chlepakov@gmail.com>
2016-07-03 13:57:05 +0200

    -Added function to get csrf_cookie for future use

--- app/static/app/index-app.js
+++ app/static/app/index-app.js
@@ -7,7 +7,6 @@ console.log("angular code loaded.");
 
 var donaaldaApp = angular
         .module('donaaldaApp', ['ngAria', 'ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages', 'md-steppers'])
-
     ;
 
 donaaldaApp.controller('questionController', questionController)
@@ -60,15 +59,32 @@ function questionController($scope, $q, $timeout) {
         {step: 3, completed: false, optional: false, data: {}},
     ];
 
+    vm.getCookie = function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = vm.getCookie('csrftoken');
+
     vm.post = function post(path) {
         var method = "post"; // Set method to post by default if not specified.
         var params = vm.stepData;
         // The rest of this code assumes you are not using a library.
         // It can be made less wordy if you use one.
         var form = document.createElement("form");
+        document.cookie = csrftoken;
         form.setAttribute("method", method);
         form.setAttribute("action", path);
-        console.log(vm.stepData.toString);
         for (var key in params) {
             if (params.hasOwnProperty(key)) {
                 var hiddenField = document.createElement("input");
@@ -79,8 +95,8 @@ function questionController($scope, $q, $timeout) {
                 form.appendChild(hiddenField);
             }
         }
+        console.log(csrftoken)
         document.body.appendChild(form);
-        console.log('GO');
         form.submit();
     }
 
