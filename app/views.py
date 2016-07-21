86368863177509f08c9564f1d8741bde6717007a
michaelch <michael.chlepakov@gmail.com>
2016-07-21 22:48:19 +0200

    Refactoring to viewsets instead

--- app/views.py
+++ app/views.py
@@ -14,9 +14,25 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_http_methods
 from django.http import *
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .serializers import *
 from .permissions import *
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'categories': reverse('category-list', request=request, format=format),
+    })
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 
 class CategoryList(generics.ListCreateAPIView):
