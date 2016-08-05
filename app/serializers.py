cd571f3b0bab14eecea2e63fefbb955993c1d21d
michaelch <michael.chlepakov@gmail.com>
2016-08-05 11:25:05 +0200

    Added full references to serialized objects, ie no multiple requests necessary to get all required data for each data object

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('pk', 'code', 'name',)
+
+
+class ProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ('pk', 'name', 'is_visible',)
+
+
+class ProviderProfileSerializer(serializers.ModelSerializer):
+    owner = ProviderSerializer()
+
+    class Meta:
+        model = ProviderProfile
+        fields = ('pk',
+                  'public_name', 'url_name', 'logo_image', 'profile_image', 'banner_image', 'introduction',
+                  'contact_email',
+                  'website', 'owner',)
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -8,7 +38,38 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('pk', 'name', 'picture', 'backgroundPicture', 'short_description', 'description', 'iconString',)
 
 
+class ProductSerializer(serializers.ModelSerializer):
+    provider = ProviderSerializer()
+    tags = TagSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Product
+        fields = ('pk',
+                  'name', 'provider', 'product_type', 'serial_number', 'description', 'specifications', 'image1',
+                  'image2',
+                  'image3', 'end_of_life', 'tags',)
+
+
+class ProductSetSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(read_only=True, many=True)
+    tags = TagSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ProductSet
+        fields = ('pk', 'name', 'description', 'products', 'creator', 'tags',)
+
+
+class ProductTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductType
+        fields = ('pk', 'type_name',)
+
+
 class ScenarioSerializer(serializers.ModelSerializer):
+    provider = ProviderSerializer()
+    scenario_product_set = ProductSetSerializer()
+    categories = CategorySerializer(read_only=True, many=True)
+
     class Meta:
         model = Scenario
         fields = (
@@ -23,97 +84,75 @@ class ScenarioDescriptionSerializer(serializers.ModelSerializer):
         fields = ('pk', 'belongs_to_scenario', 'description', 'image', 'left_right', 'order',)
 
 
-class ProductSetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductSet
-        fields = ('pk', 'name', 'description', 'products', 'creator', 'tags',)
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('pk',
-                  'name', 'provider', 'product_type', 'serial_number', 'description', 'specifications', 'image1',
-                  'image2',
-                  'image3', 'end_of_life', 'tags',)
-
-
-class ProductTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductType
-        fields = ('pk', 'type_name',)
-
-
-class ProviderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Provider
-        fields = ('pk', 'name', 'is_visible',)
-
-
-class ProviderProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProviderProfile
-        fields = ('pk',
-                  'public_name', 'url_name', 'logo_image', 'profile_image', 'banner_image', 'introduction',
-                  'contact_email',
-                  'website', 'owner',)
-
-
 class EmployeeSerializer(serializers.ModelSerializer):
+    employer = ProviderSerializer()
+
     class Meta:
         model = Employee
         fields = ('pk', 'employer',)
 
 
 class UserImageSerializer(serializers.ModelSerializer):
+    belongs_to_user = UserSerializer()
+
     class Meta:
         model = UserImage
         fields = ('pk', 'belongs_to_user', 'image',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    comment_from = UserSerializer()
+
     class Meta:
         model = Comment
         fields = ('pk', 'comment_from', 'comment_title', 'comment_content', 'rating', 'creation_date', 'page_url',)
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ('pk', 'question_text', 'answer_presentation', 'order', 'answer_set')
-
-
 class AnswerSerializer(serializers.ModelSerializer):
+    tag = TagSerializer()
+
     class Meta:
         model = Answer
         fields = ('pk', 'belongs_to_question', 'answer_text', 'tag',)
 
 
-class TagSerializer(serializers.ModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
+    answer_set = AnswerSerializer(read_only=True, many=True)
+
     class Meta:
-        model = Tag
-        fields = ('pk', 'code', 'name',)
+        model = Question
+        fields = ('pk', 'question_text', 'answer_presentation', 'order', 'answer_set')
 
 
 class GivenAnswersSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    user_answer = AnswerSerializer(read_only=True, many=True)
+
     class Meta:
         model = GivenAnswers
         fields = ('pk', 'user', 'user_answer',)
 
 
 class QuestionSetSerializer(serializers.ModelSerializer):
+    question = QuestionSerializer(read_only=True, many=True)
+    category = CategorySerializer()
+
     class Meta:
         model = QuestionSet
         fields = ('pk', 'name', 'question', 'category', 'order',)
 
 
 class SessionTagsSerializer(serializers.ModelSerializer):
+    tag = TagSerializer(read_only=True, many=True)
+
     class Meta:
         model = SessionTags
         fields = ('pk', 'session', 'tag',)
 
 
 class QuestionStepSerializer(serializers.ModelSerializer):
+    question_steps = QuestionSetSerializer(read_only=True, many=True)
+
     class Meta:
         model = QuestionStep
         fields = ('pk', 'name', 'question_steps',)
