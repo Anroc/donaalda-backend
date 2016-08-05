13ef22e877c727e88a89eae0cf739b1059ad6ca7
Michael Chlepakov <michael.chlepakov@gmail.com>
2016-08-05 13:19:28 +0200

    Changed pagination size to 5

--- advisor/settings.py
+++ advisor/settings.py
@@ -181,5 +181,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
 }
