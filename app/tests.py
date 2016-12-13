496fbe2b26ae4996fc2d5ba2013bb4d266595da0
Tim Malte Gräfje <Tim.Graefje@gmx.de>
2016-12-13 13:32:11 +0100

    Add unit test to verify the v1 list endpoints
    It currently walks through the schema and verifies that every list
    endpoint in the `/v1/` category has no required arguments and returns
    a success code. It does not check if they return the correct results.

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+
 from django.test import Client
 from django.contrib.auth.models import User
 from django.contrib import auth
+from rest_framework.schemas import SchemaGenerator
+
 from .models import Category, Product
 
 
@@ -47,3 +50,26 @@ class SimpleTest(unittest.TestCase):
             response = self.client.post('/app/logout', {'username': un, 'password': pw})  # send logout request
             user = auth.get_user(self.client)
             assert not user.is_authenticated()  # should not be logged in anymore
+
+
+class SchemaTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        # get the api schema
+        generator = SchemaGenerator()
+        self.schema = generator.get_schema()
+
+    def test_v1_endpoints(self):
+        v1_endpoints = self.schema['v1']
+
+        for name, obj in v1_endpoints.items():
+            for method, link in obj.items():
+                # only test 'list' endpoints for now
+                if method != 'list':
+                    continue
+
+                print("Testing endpoint %s (%s)" % (name, method))
+                self.assertFalse(any(field.required for field in link.fields))
+                response = getattr(self.client, link.action)(link.url)
+                self.assertEqual(response.status_code, 200)
