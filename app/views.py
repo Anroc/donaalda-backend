96ad71974cff6a0d6a285d0471c84f0ff2f1b6b8
Tim Malte Gräfje <tim.graefje@gmx.de>
2017-01-10 21:39:25 +0100

    Refactor Scenario -> Shopping basket partitioning
    This was necessary because The FinalProductList endpoint did already
    receive the shopping basket that it should implement but did not try to
    implement it itself if it wasn't in the cache.
    
    This commit fixes this by adding code to the View that regenerates the
    shopping basket implementation if it was not found in cache

--- app/views.py
+++ app/views.py
@@ -16,8 +16,10 @@ from rest_framework import viewsets
 from rest_framework.decorators import *
 from rest_framework.response import Response
 
-from .logic import implement_scenarios
-from .logic.sorting import sort_scenarios
+from .logic import (
+        partition_scenarios,
+        sort_scenarios,
+        implement_scenarios)
 from .forms import LoginForm
 from .permissions import *
 from .serializers import *
@@ -25,7 +27,6 @@ from .validators import *
 from .suggestions import SuggestionsInputSerializer, ScenarioImpl, SuggestionsOutputSerializer, SuggestionsPagination, \
     WeAreRESTfulNowException, InvalidShoppingBasketException
 from .final_product_list import FinalProductListSerializer, FinalProductListElement, NoShoppingBasketException
-from .constants import SHOPPING_BASKET_SCENARIO_ID
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
@@ -142,22 +143,9 @@ class Suggestions(generics.ListAPIView):
         input_serializer.is_valid(raise_exception=True)
         suggestions_input = input_serializer.save()
 
-        scenario_ids = set()
-        scenarios = Scenario.objects.prefetch_related(
-            'meta_broker__implementation_requires',
-            'meta_endpoints__implementation_requires'
-        ).all()
-        for basket_elem in suggestions_input.shopping_basket:
-            scenario_ids.add(basket_elem[SHOPPING_BASKET_SCENARIO_ID])
-
-        shopping_basket = set()
-        sorting_scenarios = set()
-        for scenario in scenarios:
-            if scenario.pk in scenario_ids:
-                shopping_basket.add(scenario)
-            else:
-                sorting_scenarios.add(scenario)
-
+        # partition scenarios into shopping basket and suggestable
+        shopping_basket, sorting_scenarios = partition_scenarios(
+                suggestions_input.shopping_basket)
         # call scenario sorting
         sorted_tuple_list = sort_scenarios(sorting_scenarios, suggestions_input)
 
@@ -191,12 +179,20 @@ class FinalProductList(generics.ListAPIView):
 
         input_serializer = SuggestionsInputSerializer(data=self.request.data)
         input_serializer.is_valid(raise_exception=True)
+        suggestions_input = input_serializer.save()
+
         device_mapping = cache.get(
             hash(str(input_serializer.data)),
             None
         )
+
         if device_mapping is None:
-            raise NoShoppingBasketException
+            shopping_basket, unused = partition_scenarios(
+                    suggestions_input.shopping_basket)
+            old_product_set, device_mapping = implement_scenarios(
+                    shopping_basket, suggestions_input)
+            if not old_product_set:
+                raise NoShoppingBasketException
         return [
                 FinalProductListElement(product,
                                         [scenario.id for scenario in scenarios]
