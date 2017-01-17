1b5dfe35603bad76e124d0b8795bf1119d952674
Tim Malte Gräfje <Tim.Graefje@gmx.de>
2017-01-17 18:26:11 +0100

    Add a test for the rating fixup on Scenario.save()
    It tests that no matter if a scenario was rated in zero, one, two or all
    categories previously, after the call to .save(), it should be rated in
    all categories exactly once (no two ratings for the same category)

--- app/tests.py
+++ app/tests.py
@@ -2,8 +2,10 @@
 
 import unittest
 
-from django.test import Client
+from django.test import TestCase
 from rest_framework.schemas import SchemaGenerator
+
+from .models import Provider, Scenario, Category, ScenarioCategoryRating
 
 
 class IgnoreFilterFieldsSchemaGenerator(SchemaGenerator):
@@ -11,10 +13,8 @@ class IgnoreFilterFieldsSchemaGenerator(SchemaGenerator):
         return []
 
 
-class SchemaTest(unittest.TestCase):
+class SchemaTest(TestCase):
     def setUp(self):
-        # Every test needs a client.
-        self.client = Client()
         # get the api schema
         generator = IgnoreFilterFieldsSchemaGenerator()
         self.schema = generator.get_schema()
@@ -28,7 +28,62 @@ class SchemaTest(unittest.TestCase):
                 if method != 'list':
                     continue
 
-                print("Testing endpoint %s (%s)" % (name, method))
+                #print("Testing endpoint %s (%s)" % (name, method))
                 self.assertFalse(any(field.required for field in link.fields))
                 response = getattr(self.client, link.action)(link.url)
                 self.assertEqual(response.status_code, 200)
+
+
+class ScenarioCategoryRatingFixupTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        provider = Provider(public_name="testprovider")
+        provider.save()
+
+        # generate 4 test categories (accessible via selv.c0,... later)
+        for cn in ["c1", "c2", "c3", "c4"]:
+            setattr(cls, cn, Category(name=cn))
+            getattr(cls, cn).save()
+
+        # generate 4 test scenarios with 0,1,2 or all categories rated
+        # respectively
+        for sn in ["s0", "s1", "s2", "s4"]:
+            setattr(cls, sn, Scenario(name=sn, provider=provider))
+            getattr(cls, sn).save()
+
+        # delete all ScenarioCategoryRatings that may have been created
+        ScenarioCategoryRating.objects.all().delete()
+
+        # create the ratings for the scenarios
+        for n in [0, 1, 2, 4]:
+            sn = "s%d" % n
+            for k in range(1,n+1):
+                cn = "c%d" % k
+                ScenarioCategoryRating(scenario=getattr(cls, sn), category=getattr(cls, cn), rating = 3).save()
+
+    def _assert_database_integrity(self, scenario):
+        # save the scenario to trigger the fixup
+        scenario.save()
+
+        # get the lists of rated categories (as ids)
+        scrs_manager = scenario.scenariocategoryrating_set
+        rated_categories = scrs_manager.values_list('category', flat=True)
+
+        # assert that our scenario has been rated in every category
+        self.assertEqual(set(rated_categories),
+                         set(Category.objects.values_list('pk', flat=True)))
+
+        # assert that there is exactly one rating for every category
+        self.assertEqual(len(rated_categories), Category.objects.count())
+
+    def testNoRatings(self):
+        self._assert_database_integrity(self.s1)
+
+    def testOneRating(self):
+        self._assert_database_integrity(self.s1)
+
+    def testTwoRatings(self):
+        self._assert_database_integrity(self.s2)
+
+    def testAllRatings(self):
+        self._assert_database_integrity(self.s4)
