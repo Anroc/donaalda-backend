c43e311630022ebe55d21ce96cd821099d881023
michaelch <michael.chlepakov@gmail.com>
2017-01-25 21:20:05 +0100

    First draft without discussion/feedback, still waiting for people to respond until actual implementation

--- app/models.py
+++ app/models.py
@@ -100,6 +100,8 @@ class Scenario(models.Model):
                                                    verbose_name="Dieses Szenario liegt im Warenkorb von Session",
                                                    blank=True)
     thumbnail = ImageSpecField(source='picture', processors=[ResizeToFill(200, 100)], format='JPEG')
+    subcategory_descriptor = models.CharField(max_length=255, default="---")
+    short_description = models.CharField(max_length=255, default="---")
 
     def __str__(self):
         return '%s' % self.name
@@ -107,9 +109,10 @@ class Scenario(models.Model):
     def save(self, *args, **kwargs):
         """Saves an instance of a Scenario and sets url_name to a cleaned version of name"""
         self.url_name = url_alias(self.name)
+        self.short_description = "%s - %s" % (self.subcategory, self.subcategory_descriptor)
         super(Scenario, self).save(*args, **kwargs)
 
-        # for each rating create an rating for this element
+        # for each rating create a rating for this element
         # first, get the categories that this scenario is not rated in
         unrated_categories = Category.objects.exclude(
                 pk__in=self.categories.values_list('pk', flat=True))
